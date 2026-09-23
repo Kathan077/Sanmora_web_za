50f9e9165537820986d2b795d260fe3b5d840923
Kathan Patel <kathanpatel099@gmail.com>
2026-09-23 18:03:23 +0530

    feat: Add multiple email contact links to Footer component

--- frontend-sanmora/components/Footer/Footer.jsx
+++ frontend-sanmora/components/Footer/Footer.jsx
@@ -67,13 +67,22 @@ export default function Footer() {
               <h4 className={styles.presenceTitle}>Office Address</h4>
             </div>
 
-            <a href="mailto:info@sanmora.in" className={styles.emailPill}>
-              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
-                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-                <polyline points="22,6 12,13 2,6" />
-              </svg>
-              info@sanmora.in
-            </a>
+            <div className={styles.emailGroup}>
+              <a href="mailto:info@sanmora.in" className={styles.emailPill}>
+                <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
+                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+                  <polyline points="22,6 12,13 2,6" />
+                </svg>
+                info@sanmora.in
+              </a>
+              <a href="mailto:info@sanmora.co.za" className={styles.emailPill}>
+                <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
+                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+                  <polyline points="22,6 12,13 2,6" />
+                </svg>
+                info@sanmora.co.za
+              </a>
+            </div>
           </div>
 
           <div className={styles.officesGrid}>
